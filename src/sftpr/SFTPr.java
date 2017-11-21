759f30d1c13ada89435b2d58e320fd924b7c2629
Salva Corts <salvacortsit@gmail.com>
2017-11-21 10:52:29 +0100

    first commit

---
+++ src/sftpr/SFTPr.java
@@ -0,0 +1,16 @@
+package sftpr;
+
+/**
+ *
+ * @author salva
+ */
+public class SFTPr {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
